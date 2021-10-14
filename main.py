157e6a1c7483fe245044c1e8ccc45e95f28708b3
iviand2 <iviand2@gmail.com>
2021-10-14 12:39:08 +0300

    initial

--- main.py
+++ main.py
@@ -1,16 +1,11 @@
-# This is a sample Python script.
+import requests
+import pandas as pd
+import config
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def start():
+    print(f'Hi')  # Press Ctrl+F8 to toggle the breakpoint.
 
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    start()
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
